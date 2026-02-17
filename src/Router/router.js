564c7fa51ebf660d3a83170d4467c081c171b92a
polokrf <polokkumar9030@gmail.com>
2026-02-17 11:19:07 +0600

    delet my dupcate file

--- src/Router/router.js
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router";
-import Root from "../Layout/Root";
-
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    Component:Root 
-  }
-])
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider } from 'react-router'
-import { router } from './Router/router.js'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}>
+   
       <App />
-    </RouterProvider>
-  </StrictMode>,
+   
+  </StrictMode>
 );
