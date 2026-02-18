31f099446e453827eb144fa8d281bb57f7f29f14
polokrf <polokkumar9030@gmail.com>
2026-02-18 20:33:10 +0600

    only my code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,36 +1,17 @@
 import React from 'react';
-import Hero from './Hero/Hero';
-import AcademicPrograms from '../../components/Home/AcademicPrograms';
-import NoticeBoard from '../../components/Home/NoticeBoard';
 import SecureCloudSystem from '../../components/Home/SecureCloudSystem';
 import Testimonials from '../../components/Home/Testimonials';
-import Container from '../../Layouts/Container';
-import Collaboration from '../../components/Home/Collaboration';
-import SocialProof from '../../components/Home/SocialProof';
-import HowItWorks from "../../components/Home/HowItWorks";
-import FAQ from "../../components/Home/FAQ";
-import Contact from '../../components/Home/Contact';
+
 
 const Home = () => {
   return (
     <>
-      <Hero />
-      <AcademicPrograms />
-      <NoticeBoard />
+      
       {/* create by polok */}
       <SecureCloudSystem></SecureCloudSystem>
       {/* create by polok */}
       <Testimonials></Testimonials>
-      {/* Create By Emon */}
-      <Collaboration></Collaboration>
-      {/* Create By Emon */}
-      <SocialProof></SocialProof>
-      {/* created by Peyas */}
-      <HowItWorks />
-      {/* created by peyas */}
-      <FAQ />
-      {/* created by mahialam */}
-      <Contact />
+     
     </>
   );
 };
