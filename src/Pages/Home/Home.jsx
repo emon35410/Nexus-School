506b2626941a37c6ff5117d0ca9f770c6cef1072
koushik-21 <koushik.biswas.edu@gmail.com>
2026-02-18 18:50:46 +0600

    hello3

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,11 @@ import Hero from "./Hero/Hero";
 import AcademicPrograms from "../../components/Home/AcademicPrograms";
 import NoticeBoard from "../../components/Home/NoticeBoard";
 import SecureCloudSystem from "../../components/Home/SecureCloudSystem";
-<<<<<<< HEAD
 import LogoMarquee from "../../components/Home/LogoMarquee";
-=======
 import Testimonials from "../../components/Home/Testimonials";
 import Container from "../../Layouts/Container";
 import HowItWorks from "../../components/Home/HowItWorks";
 import FAQ from "../../components/Home/FAQ";
->>>>>>> 58737753490fb02d7491476b5b9ab1c4f1760cb5
 
 const Home = () => {
   return (
