73f59b31226bedb5676661bfa3782606df5cc8f4
koushik-21 <koushik.biswas.edu@gmail.com>
2026-02-23 11:31:28 +0600

    hello

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Hero from './Hero/Hero';
-import AcademicPrograms from '../../components/Home/AcademicPrograms';
-import NoticeBoard from '../../components/Home/NoticeBoard';
-import SecureCloudSystem from '../../components/Home/SecureCloudSystem';
-import Testimonials from '../../components/Home/Testimonials';
-import Container from '../../Layouts/Container';
-import Collaboration from '../../components/Home/Collaboration';
-import SocialProof from '../../components/Home/SocialProof';
+import React from "react";
+import Hero from "./Hero/Hero";
+import AcademicPrograms from "../../components/Home/AcademicPrograms";
+import NoticeBoard from "../../components/Home/NoticeBoard";
+import SecureCloudSystem from "../../components/Home/SecureCloudSystem";
+import Testimonials from "../../components/Home/Testimonials";
+import Container from "../../Layouts/Container";
+import Collaboration from "../../components/Home/Collaboration";
+import SocialProof from "../../components/Home/SocialProof";
 import HowItWorks from "../../components/Home/HowItWorks";
 import FAQ from "../../components/Home/FAQ";
+import LogoMarquee from "../../components/Home/LogoMarquee";
 
 const Home = () => {
   return (
